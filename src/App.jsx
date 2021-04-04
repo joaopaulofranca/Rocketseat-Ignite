ef2e28850d639f1d72517fe14e1c6f178ed9cd0d
joaopaulofranca <joaopaulofranca211@gmail.com>
2021-04-04 16:51:58 -0300

    Importaçao de arquivos css

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
+import './styles/global.css';
+
 export function App(){
-    throw new Error('Seeeloko se lascou !!!');
+    // throw new Error('Seeeloko se lascou !!!');
 
     return <h1>Ola Mundo JS</h1>
 }
