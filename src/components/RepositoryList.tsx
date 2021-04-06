9720f6cf81f0e613069d936abf912df7e62bb0ad
joaopaulofranca <joaopaulofranca211@gmail.com>
2021-04-06 20:47:32 -0300

    Atualizando codigo

--- src/components/RepositoryList.tsx
+++ src/components/RepositoryList.tsx
@@ -2,9 +2,15 @@ import { useState, useEffect } from 'react';
 import { RepositoryItem } from "./RepositoryItem";
 import '../styles/repositories.scss';
 
+interface Repository {
+        name: string;
+        description: string; 
+        html_url: string;
+}
+
 // https://api.github.com/users/joaopaulofranca/repos
 export function RepositoryList(){
-    const [repositories, setRepositories] = useState([]);
+    const [repositories, setRepositories] = useState<Repository[]>([]);
 
     useEffect(() => {
         fetch(' https://api.github.com/users/joaopaulofranca/repos')
