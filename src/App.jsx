9b40a5456094cbb5721fba61e79b57f48162c11e
joaopaulofranca <joaopaulofranca211@gmail.com>
2021-04-05 22:24:49 -0300

    Contador

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
+import { Counter } from './components/Counter';
 import { RepositoryList } from './components/RepositoryList';
 import './styles/global.scss';
 
 export function App(){
-    // throw new Error('Seeeloko se lascou !!!');
+   
 
-    return <RepositoryList />
+    return(
+        <>
+        <RepositoryList />
+        <Counter />
+        </>
+    );
 }
---
+++ src/components/RepositoryItem.jsx
@@ -0,0 +1,11 @@
+export function RepositoryItem(props) {
+    return (
+        <li>
+            <strong>unform</strong>
+            <p>Form in React</p>
+            <a href="">
+                Acessar repositório
+            </a>
+        </li>
+    );
+}
