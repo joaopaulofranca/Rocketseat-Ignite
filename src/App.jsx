8eff753a42e35d5bbe8fdc657b0d1a442808be85
joaopaulofranca <joaopaulofranca211@gmail.com>
2021-04-05 21:49:36 -0300

    RepositoryList

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
+import { RepositoryList } from './components/RepositoryList';
 import './styles/global.scss';
 
 export function App(){
     // throw new Error('Seeeloko se lascou !!!');
 
-    return <h1>Ola Mundo JS</h1>
+    return <RepositoryList />
 }
